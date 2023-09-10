92f3f225c25a4ed44f55873dfe94431c58e4d9df
Darby Ford <pippabuttertubs@gmail.com>
2023-09-10 16:39:48 -0400

    Refactored some more

--- PetStoreNew/Program.cs
+++ PetStoreNew/Program.cs
@@ -21,16 +21,8 @@ namespace PetStore
 
             string runProgram = "yes";
 
-            while (runProgram.ToLower() == "yes")
-            { 
-
-            Console.WriteLine("Press 1 to enter a product.");
-            Console.WriteLine("Press 2 to look up a product.");
-            Console.WriteLine("Press 3 to view all in stock products.");
-            Console.WriteLine("Press 4 to see the total cost of every in stock product.");
-            Console.WriteLine("Type 'exit' to quit. ");
-
-            string userInput = Console.ReadLine();
+            
+                string userInput = ProgramPrompts();
 
                 while (userInput.ToLower() != "exit")
                 {
@@ -52,13 +44,11 @@ namespace PetStore
                             default:
                                 break;
                         }
-                        break;
                     }
 
                     else if (userInput == "2")
                     {
                         DictionaryLookup.Lookup((ProductLogic)productLogic);
-                        break;
                     }
 
                     else if (userInput == "3")
@@ -68,13 +58,12 @@ namespace PetStore
                         {
                             Console.WriteLine(inStockProduct);
                         }
-                        break;
                     }
 
-                     else if (userInput == "4")
+                    else if (userInput == "4")
                     {
                         Console.WriteLine($"The total price of everything in stock is ${productLogic.GetTotalPriceOfInventory()}.");
-                        break;
+                      
                     }
 
                     else if (userInput.ToLower() == "exit ")
@@ -85,21 +74,27 @@ namespace PetStore
                     else
                     {
                         Console.WriteLine("That answer is not valid.");
-                        break;
                     }
-                }
+                
 
-                Console.Write("Would you like to continue using the program? Type \"Yes\" or \"No\": ");
-                runProgram = Console.ReadLine();
-                while (runProgram.ToLower() != "yes" && runProgram.ToLower() != "no")
-                {
-                    Console.WriteLine("What was that? Did you say \"Yes\" or \"No\"? ");
-                    runProgram = Console.ReadLine();
-                }
+                userInput = ProgramPrompts();
             }
             Console.WriteLine("Thank you for visiting your local pet store!");
             Console.ReadKey();
         }
+
+        private static string ProgramPrompts()
+        {
+            Console.WriteLine("Press 1 to enter a product.");
+            Console.WriteLine("Press 2 to look up a product.");
+            Console.WriteLine("Press 3 to view all in stock products.");
+            Console.WriteLine("Press 4 to see the total cost of every in stock product.");
+            Console.WriteLine("Type 'exit' to quit. ");
+
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
         static IServiceProvider CreateServiceCollection()
         {
             return new ServiceCollection()
